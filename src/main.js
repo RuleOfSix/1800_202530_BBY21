d3f9f8b8fab11326725686e2c696f086fb75cf5f
Min <79357141+mjtwo20@users.noreply.github.com>
2025-10-26 19:06:08 -0700

    add mockup calendar view

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css"; //webpack uses file-loader to handle font files
 import "bootstrap";
 import "../styles/style.css";
 
-function sayHello() {}
-// document.addEventListener('DOMContentLoaded', sayHello);
+// Calendar.js
+import "./calendar.js";
