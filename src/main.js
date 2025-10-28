56ddbc6440323e52aad76cddb52604b376b37306
June Pyle <github@ruleofsix.ca>
2025-10-27 23:30:56 -0700

    Create navbar and footer components, update calendar.html and index.html to use navbar and footer components

--- src/main.js
+++ src/main.js
@@ -2,6 +2,4 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css"; //webpack uses file-loader to handle font files
 import "bootstrap";
 import "../styles/style.css";
-
-// Calendar.js
 import "./calendar.js";
