6053b034e2d403bffb083aae5318102a5cf979e1
Justin Watson <justinwatson480@gmail.com>
2025-10-29 12:47:32 -0700

    Created firebase login, made checklist

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css"; //webpack uses file-loader to handle font files
 import "bootstrap";
 import "../styles/style.css";
+
 import "./calendar.js";
