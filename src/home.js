a44d0bad4a3c4166f91f1e565c20ebb5d3a0369f
June Pyle <github@ruleofsix.ca>
2025-11-02 16:42:17 -0800

    Finalized group creation logic

--- src/home.js
+++ src/home.js
@@ -9,19 +9,23 @@ const nameInput = groupCreationMenu.querySelector("#groupName");
 const submitButton = groupCreationMenu.querySelector("#submitGroup");
 let uid;
 
-addGroupButton.addEventListener("click", () => {
-  groupCreationMenu.hidden = !groupCreationMenu?.hidden;
-  darkeningScreen.hidden = !darkeningScreen?.hidden;
-});
+addGroupButton.addEventListener("click", toggleGroupCreationMenu);
 
 submitButton.addEventListener("click", () => {
   const groupName = nameInput?.value?.trim() ?? "";
   addDoc(collection(db, "groups"), {
+    name: groupName,
     taskIDs: [],
     userIDs: [uid],
   });
+  toggleGroupCreationMenu();
 });
 
 onAuthReady((user) => {
   uid = user.uid;
 });
+
+function toggleGroupCreationMenu() {
+  groupCreationMenu.hidden = !groupCreationMenu?.hidden;
+  darkeningScreen.hidden = !darkeningScreen?.hidden;
+}
